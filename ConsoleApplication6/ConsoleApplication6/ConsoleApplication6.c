27b310a2b22d8d72dff730ab7100af02c3556640
fizerrr <birddosky22@gmail.com>
2022-03-18 12:57:27 +0100

    Fix many problems and bugs

--- ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.c
+++ ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.c
@@ -1,4 +1,5 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+#define file_name "db.txt"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,13 +30,15 @@ typedef struct list
 void start(list* L);
 
 int push_back(list* L, char* title_new, char* staging_new, char* type_new, char* music_new, int year_of_production_new, int time_new);
-void loaddb(list* L, char* title_new, char* staging_new, char* type_new, char* music_new, int year_of_production_new, int time_new);
+void loaddb(list* L);
 
 void print_list(list* L);
 
 void insertdata(char* title_new, char* staging_new, char* type_new, char* music_new, int* year_of_production_new, int* time_new);
 
 int print_wanteds(list* L, char* wanted);
+
+int howlong(list* L);
 
 void savedb(list* L);
 
@@ -101,8 +104,11 @@ int main()
         savedb(&dvds);
         break;
 
+    case 5:
+        loaddb(&dvds);
+        break;
 
-
+   
 
     default:
         printf("d*\n");
@@ -144,7 +150,9 @@ void menu() {
     printf("(1)DODAJ POZYCJEA\n");
     printf("(2)WYŚWIETL WSZYSTKIE POZCYJE\n");
     printf("(3)WYSZUKAJ POZYCJEC\n");
-    printf("(4)Prawidlowe zamkniecie programu\n");
+    printf("(4)save and exit\n");
+    printf("(5)load\n");
+    printf("(6)size test\n");
     printf("\n");
     printf("Wybierz numer odpowiadajacy oczzekiwanej operacji...\n");
 }
@@ -203,7 +211,7 @@ void print_list(list* L)
     {
       
         
-         printf("%s %s %s %s %d %d", traverse->data->title, traverse->data->staging, traverse->data->type, traverse->data->music, traverse->data->time, traverse->data->year_of_production);
+         printf("%s %s %s %s %d %d", traverse->data->title, traverse->data->staging, traverse->data->type, traverse->data->music, traverse->data->year_of_production, traverse->data->time);
             printf("\n");
             traverse = traverse->next;
         
@@ -215,7 +223,7 @@ void print_list(list* L)
 
 int print_wanteds(list* L,char* wanted)
 {
-    node* traverse = L->head;\
+    node* traverse = L->head;
     printf("Title:  Staging:  Type:  Music:  Year of production:  Time: \n");
     while (traverse != NULL)
     {
@@ -223,7 +231,7 @@ int print_wanteds(list* L,char* wanted)
         
         if (strcmp(suspect,wanted) ==0)
         {
-            printf("%s %s %s %s %d %d", traverse->data->title, traverse->data->staging, traverse->data->type, traverse->data->music, traverse->data->time, traverse->data->year_of_production);
+            printf("%s %s %s %s %d %d", traverse->data->title, traverse->data->staging, traverse->data->type, traverse->data->music, traverse->data->year_of_production, traverse->data->time);
             printf("\n");
             return 1;
         }
@@ -240,13 +248,15 @@ int print_wanteds(list* L,char* wanted)
 void savedb(list* L)
 {
     node* traverse = L->head;
+    int tmp = 0;
+    tmp = howlong(&L);
 
     FILE* file;
-    file = fopen("db.txt", "w");
-  
+    file = fopen(file_name, "w");
+    fprintf(file,"%d\n",tmp);
         while (traverse != NULL)
         {
-            fprintf(file,"%s\n%s\n%s\n%s\n%d\n%d\n", traverse->data->title, traverse->data->staging, traverse->data->type, traverse->data->music, traverse->data->time, traverse->data->year_of_production);
+            fprintf(file,"%s\n%s\n%s\n%s\n%d\n%d\n", traverse->data->title, traverse->data->staging, traverse->data->type, traverse->data->music, traverse->data->year_of_production, traverse->data->time);
             traverse = traverse->next;
         }
         exit(1);
@@ -254,48 +264,85 @@ void savedb(list* L)
         
 }
    
-void loaddb(list* L, char* title_new, char* staging_new, char* type_new, char* music_new, int year_of_production_new, int time_new)
+void loaddb(list* L)
 {
+    char* title_new = malloc(sizeof(char) * 100);
+    char* staging_new = malloc(sizeof(char) * 100);
+    char* type_new = malloc(sizeof(char) * 100);
+    char* music_new = malloc(sizeof(char) * 100);
+    int year_of_production_new;
+    int time_new;
+    int size;
+
+    
+
+
+
+    FILE* file;
+    file = fopen(file_name, "r");
     node* new_node = malloc(sizeof(node));
     dvd* nowy = malloc(sizeof(dvd));
-    if (!new_node || !nowy)
+    fscanf(file, "%d\n", &size);
+    size = size * 5;
+    for(int i = 0;i<10;i++)
     {
-        free(new_node);
-        free(nowy);
-        return 0;
+        
+
+        if (!new_node || !nowy)
+        {
+            free(new_node);
+            free(nowy);
+            return 0;
+        }
+        fscanf(file, "%s\n", title_new);
+        fscanf(file, "%s\n", staging_new);
+        fscanf(file, "%s\n", type_new);
+        fscanf(file, "%s\n", music_new);
+        fscanf(file, "%d\n", &year_of_production_new);
+        fscanf(file, "%d\n", &time_new);
+        
+
+        strcpy(nowy->title, title_new);
+        strcpy(nowy->staging, staging_new);
+        strcpy(nowy->type, type_new);
+        strcpy(nowy->music, music_new);
+        nowy->year_of_production = year_of_production_new;
+        nowy->time = time_new;
+
+
+        new_node->data = nowy;
+
+        if (L->head == NULL)
+        {
+            L->head = new_node;
+            L->head->next = NULL;
+            L->tail = new_node;
+            L->tail->prev = NULL;
+
+        }
+        else
+        {
+            new_node->prev = L->tail;
+            new_node->next = NULL;
+            L->tail->next = new_node;
+            L->tail = new_node;
+        }
+        
     }
-
-    strcpy(nowy->title, title_new);
-    strcpy(nowy->staging, staging_new);
-    strcpy(nowy->type, type_new);
-    strcpy(nowy->music, music_new);
-    nowy->year_of_production = year_of_production_new;
-    nowy->time = time_new;
-
-
-    new_node->data = nowy;
-
-    if (L->head == NULL)
-    {
-        L->head = new_node;
-        L->head->next = NULL;
-        L->tail = new_node;
-        L->tail->prev = NULL;
-
-    }
-    else
-    {
-        new_node->prev = L->tail;
-        new_node->next = NULL;
-        L->tail->next = new_node;
-        L->tail = new_node;
-    }
-    
+    fclose(file);
 }
 
+int howlong(list* L) {
+    node* traverse = L->head;
+    int records = 0;
+    while (traverse != NULL)
+    {
+        records++;
+        traverse = traverse->next;
+    }
+    return records;
 
-
-
+}
 
 
 
